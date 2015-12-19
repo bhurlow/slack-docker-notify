5baa29cac3f6c5280720c7769ac0d87532c565a3
bhurlow <bhurlow@gmail.com>
2015-12-18 21:03:32 -0800

    example script, http option, added readme

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ stream events from docker to your slack `#notifications` channel
 
 ### Usage
 
+for single docker hosts you can simply connect to to docker via the unix socket:
+
 ```
 docker run -d \
   -e SLACK_WEBHOOK_URL=$SLACK_WEBHOOK_URL \
@@ -13,4 +15,18 @@ docker run -d \
   bhurlow/slack-docker-notify 
 ```
 
+you may also provide a specific docker host (like a docker swarm master node):
 
+```
+docker run \
+  -it \
+  -e DOCKER_HOST=$(docker-machine ip default) \
+  -e DOCKER_PORT=2376 \
+  -e DOCKER_TLS_VERIFY=1 \
+  -e DOCKER_CERT_PATH=/tmp/docker-certs  \
+  -v /var/run/docker.sock:/tmp/docker.sock:ro \
+  -v /tmp/docker-certs:/tmp/docker-certs \
+  bhurlow/slack-docker-notify
+```
+
+**note**: when specify DOCKER_HOST etc, you must first copy over the certs to the host.
--- app.js
+++ app.js
@@ -6,15 +6,38 @@ var moment = require('moment')
 var request = require('superagent')
 var Docker = require('dockerode')
 
-var docker = new Docker({socketPath: '/tmp/docker.sock'});
+var DOCKER_HOST = process.env.DOCKER_HOST
+var DOCKER_PORT = process.env.DOCKER_PORT
+var DOCKER_TLS_VERIFY = process.env.DOCKER_TLS_VERIFY
+var DOCKER_CERT_PATH = process.env.DOCKER_CERT_PATH
 
-var ev = docker.getEvents(function(e, r) {
+if (DOCKER_HOST && DOCKER_TLS_VERIFY && DOCKER_CERT_PATH) {
+  debug('starting http connect to docker at: %s', DOCKER_HOST)
+  let docker = new Docker({
+    host: DOCKER_HOST,
+    port: DOCKER_PORT,
+    ca: fs.readFileSync(DOCKER_CERT_PATH + '/ca.pem'),
+    cert: fs.readFileSync(DOCKER_CERT_PATH + '/cert.pem'),
+    key: fs.readFileSync(DOCKER_CERT_PATH + '/key.pem')
+  });
+  docker.getEvents(handleEvents)
+}
+
+else {
+  let docker = new Docker({socketPath: '/tmp/docker.sock'});
+  debug('starting unix socket connection to docker at: %s', '/tmp/docker.sock')
+}
+
+// util fns
+
+function handleEvents(e, r) {
+  if (e) return console.log(e)
   r.on('data', function(d) {
     let event = d.toString()
     event = JSON.parse(event)
     sendEventToSlack(event)
   })
-})
+}
 
 function dissoc (obj, key) {
   delete obj[key]
@@ -37,14 +60,15 @@ function makeBody(msg) {
 
 function sendEventToSlack(obj) {
   debug('sending event to slack')
+  let msg = formatDockerEvent(obj)
+  debug(msg)
+  let payload = makeBody(msg)
   let url = process.env.SLACK_WEBHOOK_URL
   if (!url)  return debug('must set $SLACK_WEBHOOK_URL env var') 
   let opts = {
     hostname: process.env.SLACK_WEBHOOK_URL,
     method: 'POST', 
   }
-  let msg = formatDockerEvent(obj)
-  let payload = makeBody(msg)
   request
     .post(process.env.SLACK_WEBHOOK_URL)
     .send(payload)
